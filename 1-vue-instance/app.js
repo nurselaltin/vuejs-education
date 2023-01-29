75cb262f203296f725fe9c6d1bd94a33249c753f
nurselaltin <nursel.altin@net-bt.com.tr>
2023-01-29 23:13:50 +0300

    Events kısmına başlandı.

--- 1-vue-instance/app.js
+++ 1-vue-instance/app.js
@@ -7,8 +7,30 @@ const app = Vue.createApp({
                 url: "https://github.com/",
                 target: "_blank",
                 title: "Github"
+            },
+            boxCoords : {
+                x: 0,
+                y: 0
             }
             
         }
+    },
+    methods: {
+        changeTitle(pTitle) {
+            this.title = pTitle;
+        },
+        updateCoords(message , event){
+            console.log(message);
+          
+            const title = event.x +","+ event.y;
+            this.changeTitle(title);
+            
+            this.boxCoords = {
+                x : event.x,
+                y: event.y
+            }
+            // this.boxCoords.x = event.x;
+            // this.boxCoords.y = event.y;
+        }
     }
-}).mount("#app");;
+}).mount("#app");
